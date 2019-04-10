e9d8b6306d923595a4c588ade87bb610b41d3346
cobaf <colleobafemi@gmail.com>
2019-04-10 14:53:21 +0000

    Troisieme test

--- index.php
+++ index.php
@@ -1,4 +1,15 @@
 <?php
   echo'I\'m leaning git12';
   echo'I\'m leaning git';
+  
+  $vast = true
+  if ($vast) {
+    # code...
+    echo'I\'m leaning git12';
+    echo'I\'m leaning git13';
+    echo'I\'m leaning git14';
+    echo'I\'m leaning git15';
+    echo'I\'m leaning git16';
+    echo'I\'m leaning git17';
+  }
 ?>
